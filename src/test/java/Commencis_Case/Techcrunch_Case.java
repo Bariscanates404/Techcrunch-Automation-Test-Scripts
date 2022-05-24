3e4432a85477e054bee0c2f4f6b0e2a82afe3774
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-05-24 17:56:23 +0300

    Update Techcrunch_Case.java

--- src/test/java/Commencis_Case/Techcrunch_Case.java
+++ src/test/java/Commencis_Case/Techcrunch_Case.java
@@ -17,16 +17,19 @@ import java.util.List;
 
 public class Techcrunch_Case {
     WebDriver driver;
-    static int workingLinks = 0;  // To verify working links (at line 79)
+    static final String constantCompanyTag = "| TechCrunch";    // If the company or name changes we have to set the company browser title
+    static final int constantCompanyTagLength = 12;             // The length of company browser title
+    static int workingLinks = 0;                          // To verify working links (at line 90)
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get("https://techcrunch.com/");
     }
+
     @After
     public void Teardown() {
         driver.close();
@@ -40,49 +43,57 @@ public class Techcrunch_Case {
         List<WebElement> authorElements = driver.findElements(By.cssSelector("article header span:last-child a"));
         List<WebElement> lastestNewElements = driver.findElements(By.cssSelector("article header h2 a"));
 
-        for (WebElement each : imageElements        // List of all images on latest news list
-        ) {
+        for (WebElement each : imageElements)       // List of all images on latest news list
+        {
             Assert.assertTrue(each.isDisplayed());
             Assert.assertTrue(each.isEnabled());
         }
-        for (WebElement each : authorElements       // List of all authors on latest news list
-        ) {                                         // We only count 1 author for a news article because we plan to verify by comparing them.
+        for (WebElement each : authorElements)      // List of all authors on latest news list
+        {                                          // We only count 1 author for a news article because we plan to verify by comparing them.
             Assert.assertTrue(each.isDisplayed());
             Assert.assertTrue(each.isEnabled());
         }
-        for (WebElement each : lastestNewElements   // List of all images on latest news list
-        ) {
+        for (WebElement each : lastestNewElements)  // List of all images on latest news list
+        {
             Assert.assertTrue(each.isDisplayed());
             Assert.assertTrue(each.isEnabled());
         }
-    //  Comparing numbers of the article, image and authors to verify every article has one.
+        //  Comparing numbers of the article, image and authors to verify every article has one.
         Assert.assertEquals(imageElements.size(), authorElements.size(), lastestNewElements.size());
     }
 
     @Test
     //  To compare browser title and content title
+    //  We deleted " | Techcrunch" text part on the browser title to get the raw title to check equality
+    //  We use equals() to get highly sensitive test results as the return of that approach we had to handle
+    //  hidden characters in java with regex
     public void Test02() throws InterruptedException {
         driver.findElement(By.cssSelector("article header h2 a")).click();
         String fullContentNewsTitle = driver.findElement(By.cssSelector("article header h1")).getText();
         String browserTitle = driver.getTitle();
-        browserTitle=browserTitle.substring(0,browserTitle.length()-13);    //  We deleted " | Techcrunch" text part on the browser title to get the raw title to check equality
-        Assert.assertEquals(browserTitle,fullContentNewsTitle);             //  We use equals() to get highly sensitive test results.
+        if (browserTitle.substring(browserTitle.length() - constantCompanyTagLength).equals(constantCompanyTag)) {
+            browserTitle = browserTitle.substring(0, browserTitle.length() - (constantCompanyTagLength+1));
+            browserTitle = browserTitle.replaceAll("[/r]", "");
+            fullContentNewsTitle = fullContentNewsTitle.replaceAll("[/r]", "");
+            Assert.assertEquals(fullContentNewsTitle, browserTitle);
+        } else {
+            Assert.assertEquals(fullContentNewsTitle, browserTitle);
+        }
 
         List<WebElement> linkList = driver.findElements(By.cssSelector(".article-content>p a"));
-        for (WebElement links : linkList
-        ) {
+        for (WebElement links : linkList) {
             String url = links.getAttribute("href");
             verifyLinkActive(url);
-            Thread.sleep(1000);
+            Thread.sleep(100);
         }
         System.out.println("Total link count: " + linkList.size() + "  ||   Working link count: " + workingLinks);
         Assert.assertTrue(linkList.size() == workingLinks);
     }
 
     public static void verifyLinkActive(String linkUrl) {
-    //  We are using this method to read URL responses to check errors
-    //  Page responses 2XX: generally "OK" - 3XX: relocation/redirect "OK"
-    //  -4XX: client error(some of the pages that sends 4XX responses can be work but need attention) - 5XX: server error
+        //  We are using this method to read URL responses to check problems in links
+        //  Page responses 2XX: generally "OK" - 3XX: relocation/redirect "OK"
+        //  -4XX: client error(some of the pages that sends 4XX responses can be work but need attention) - 5XX: server error
         try {
             URL url = new URL(linkUrl);
             HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
